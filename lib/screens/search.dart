673c08680b05d3fb1b546370504e70d6be82defd
Yuri Almeida <yurialves2@gmail.com>
2021-09-27 23:16:15 -0300

    chore: search page

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram/util/explore_grid.dart';
 
 class UserSearch extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('SEARCH'),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Container(
+            padding: EdgeInsets.all(8),
+            color: Colors.grey[300],
+            child: Row(
+              children: [
+                Icon(Icons.search, color: Colors.grey[500]),
+                Container(
+                  child: Center(
+                      child: Text('Search',
+                          style: TextStyle(color: Colors.grey[500]))),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
+      body: ExploreGrid(),
     );
   }
 }
